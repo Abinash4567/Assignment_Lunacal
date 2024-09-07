35f5f614dccb78e9e9888e894b3d8b1b8454a243
Abinash4567 <abinashyadav4567@gmail.com>
2024-09-07 22:44:38 +0530

    final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import NavBar from "./components/navLiink";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { HiOutlineQuestionMarkCircle } from "react-icons/hi";
+import Image from "next/image";
+import photo from "../../public/image.jpg"
+import NavBar from "./components/navLiink";
 
 export default function Home() {
   return (
-  <div className="h-[100vh] w-[100vw] flex justify-center items-center">
+  <div className="h-[100vh] w-[100vw] flex overflow-hidden p-10">
 
-      <div className="w-[690px] h-[300px] bg-[#353c44] rounded-3xl p-4">
+    <div className="h-[50vh] w-[50vw]"></div>
+
+    <div>
+
+      <div className="w-[650px] h-[250px] bg-[#353c44] rounded-3xl p-4 overflow-hidden">
         <div className="flex">
 
 
@@ -26,7 +33,7 @@ export default function Home() {
           <div className="flex flex-col">
             <NavBar/>
 
-            <div className="mt-5 pr-4 overflow-y-scroll h-[200px] text-[#818284] text-xl font-extralight scroll">
+            <div className="mt-5 pr-4 overflow-y-scroll h-[200px] text-[#818284] text-base font-extralight scroll">
             <div className="mb-6">Hello! I’m Dave, your sales rep here from Salesforce. I’ve been working at this awesome company for 3 years now.
             </div>
 
@@ -40,6 +47,69 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      <div className="px-12"><div className="bg-[#33373a] h-1 m-3 rounded-3xl"></div></div>
+
+      <div className="w-[650px] h-[300px] bg-[#353c44] rounded-3xl p-4">
+        <div className="flex mr-6">
+
+          <div className="w-[30px] mr-3 flex flex-col justify-between">
+            <div><HiOutlineQuestionMarkCircle color="#949ca9" size={28}/></div>
+            <div className="basis-1/2 -ml-2 px-[4px]">
+              <div className="grid grid-cols-2 grid-rows-3 p-[1px]">
+                  <div className="flex justify-center items-center bg-[#4a4d54] py-[6px] mr-[2px] mb-[2px]"></div>
+                  <div className="flex justify-center items-center bg-[#4a4d54] py-[6px] mr-[2px] mb-[2px]"></div>
+                  <div className="flex justify-center items-center bg-[#4a4d54] py-[6px] mr-[2px] mb-[2px]"></div>
+                  <div className="flex justify-center items-center bg-[#4a4d54] py-[6px] mr-[2px] mb-[2px]"></div>
+                  <div className="flex justify-center items-center bg-[#4a4d54] py-[6px] mr-[2px] mb-[2px]"></div>
+                  <div className="flex justify-center items-center bg-[#4a4d54] py-[6px] mr-[2px] mb-[2px]"></div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col w-full">
+            <div className="flex justify-between">
+
+              <div className="h-14 w-32 bg-[#161616] px-[28px] pt-[14px] rounded-2xl font-extrabold text-xl">Gallery</div>
+
+              <div className="flex space-x-4">
+                <div className="bg-[#3c434b] rounded-3xl text-sm font-extrabold h-12 px-4 py-[14px] border border-[#52555c] shadow-[6px_13px_7px_4px_#212429]">+ ADD IMAGE</div>
+                <div className="flex mt-1 space-x-3">
+
+                  <div className="rounded-full h-12 w-12 bg-[#2b2e33] px-[14px] py-[14px] shadow-[-9px_-1px_13px_5px_#2d3748]">
+                    <FaArrowLeft size={20} color="#71787e"/>
+                  </div>
+
+                  <div className="rounded-full h-12 w-12 bg-[#2b2e33] px-[14px] py-[14px] shadow-[10px_-1px_13px_5px_#2d3748]">
+                    <FaArrowRight size={20} color="#71787e"/>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-5 h-[200px]">
+            <div className="mb-6 grid grid-cols-3 gap-4 place-content-stretch">
+              <div className="h-[176px] bg-red-400 basis-1/3 rounded-3xl overflow-hidden">
+                <Image src={photo} alt={"image"} object-fit="cover" fill={false}/>
+              </div>
+              <div className="h-[176px] bg-red-400 basis-1/3 rounded-3xl overflow-hidden">
+                <Image src={photo} alt={"image"} object-fit="cover" fill={false}/>
+              </div>
+              <div className="h-[176px] bg-red-400 basis-1/3 rounded-3xl overflow-hidden">
+                <Image src={photo} alt={"image"} object-fit="cover" fill={false}/>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+
+      </div>
+
+      <div className="px-12"><div className="bg-[#33373a] h-1 mt-3 rounded-3xl"></div></div>
+    </div>
+
 
   </div>);
 }
